d9879c609ad5b41ce9e09e02a18b9aff35073e81
Thiruyoge <thiruyogeshwar@gmail.com>
2023-12-10 19:20:09 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,99 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <div class="card bg-primary mt-5">
+          <div class="row p-2">
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Free</h5>
+
+                  <h2 class="card-title text-center">$0 / month</h2>
+                  <hr></hr>
+
+                  <ul class="">
+                    <li class="list-group-item">Single User</li>
+                    <li class="list-group-item">50GB Storage</li>
+                    <li class="list-group-item">Unlimited Public Projects</li>
+                    <li class="list-group-item">Community Access</li>
+                    <li class="list-group-item">Unlimited Private Projects</li>
+                    <li class="list-group-item">Dedicated Phone Support</li>
+                    <li class="list-group-item">Free Subdomain</li>
+                    <li class="list-group-item">Monthly Status Reports</li>
+                  </ul>
+
+                  <center>
+                 
+                    <button class="btn btn-primary d-flex justify-content-center">
+                      Go somewhere
+                    </button>
+                  </center>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Free</h5>
+
+                  <h2 class="card-title text-center">$0 / month</h2>
+                  <hr></hr>
+
+                  <ul class="">
+                    <li class="list-group-item">Single User</li>
+                    <li class="list-group-item">50GB Storage</li>
+                    <li class="list-group-item">Unlimited Public Projects</li>
+                    <li class="list-group-item">Community Access</li>
+                    <li class="list-group-item">Unlimited Private Projects</li>
+                    <li class="list-group-item">Dedicated Phone Support</li>
+                    <li class="list-group-item">Free Subdomain</li>
+                    <li class="list-group-item">Monthly Status Reports</li>
+                  </ul>
+
+                  <center>
+                    <button class="btn btn-primary d-flex justify-content-center">
+                      Go somewhere
+                    </button>
+                  </center>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Free</h5>
+
+                  <h2 class="card-title text-center">$0 / month</h2>
+                  <hr></hr>
+
+                  <ul class="">
+                    <li class="list-group-item">Single User</li>
+                    <li class="list-group-item">50GB Storage</li>
+                    <li class="list-group-item">Unlimited Public Projects</li>
+                    <li class="list-group-item">Community Access</li>
+                    <li class="list-group-item">Unlimited Private Projects</li>
+                    <li class="list-group-item">Dedicated Phone Support</li>
+                    <li class="list-group-item">Free Subdomain</li>
+                    <li class="list-group-item">Monthly Status Reports</li>
+                  </ul>
+
+                  <center>
+                    <button class="btn btn-primary d-flex justify-content-center">
+                      Go somewhere
+                    </button>
+                  </center>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
